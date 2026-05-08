0b980c029d3c588821fc643ce3409b591c4ab50a
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2026-05-08 06:43:50 +0200

    Add light and dark mode previews and screenshot tests across platforms

--- diApp/src/commonMain/kotlin/template/di/KoinApp.kt
+++ diApp/src/commonMain/kotlin/template/di/KoinApp.kt
@@ -1,14 +1,15 @@
 package template.di
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.AndroidUiModes
 import androidx.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
 import org.koin.dsl.KoinConfiguration
 import org.koin.plugin.module.dsl.koinConfiguration
 import template.navigation.NavApp
 import template.navigation.navigationModule
+import template.ui.common.theme.AppTheme
 
-@Preview
 @Composable
 fun KoinApp() =
     KoinApplication(
@@ -20,3 +21,11 @@ fun KoinApp() =
     ) {
         NavApp()
     }
+
+@Preview(uiMode = AndroidUiModes.UI_MODE_NIGHT_NO)
+@Composable
+internal fun KoinAppPreviewLight() = AppTheme { KoinApp() }
+
+@Preview(uiMode = AndroidUiModes.UI_MODE_NIGHT_YES)
+@Composable
+internal fun KoinAppPreviewNight() = AppTheme { KoinApp() }
